b5337796ce2628b38216b43f8877d60c43ae2a6c
Joe Bay <jbay55278@gmail.com>
2021-03-10 12:58:45 -0900

    built the basic nav bar component

---
+++ portfolio/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from "react"
+function App() {
+  return (
+    <div className = "portfolio">
+      <header>
+        this will contain the header / nav bar!
+      </header>
+
+      <section>
+      big picture area most likely, with my name probably  
+      </section>
+      <section>
+        another section with an about me area talking about me and what I do.
+      </section>
+    </div>
+  );
+}
+
+export default App;
